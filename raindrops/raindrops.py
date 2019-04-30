846086a3a2ed073faad1f7d4a52dedcedb8b6cf7
Piotr <posone89@gmail.com>
2019-04-30 13:28:18 +0200

    isogram

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,3 +1,13 @@
 def raindrops(number):
-    
+    str_list = ''
+    if number % 3 == 0:
+        str_list += 'Pling'
+    if number % 5 == 0:
+        str_list += 'Plang'
+    if number % 7 == 0:
+        str_list += 'Plong'
+    if len(str_list) == 0:
+        str_list += str(number)
+    return str_list
+
     pass
