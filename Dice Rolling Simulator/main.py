ff1adb5740e1794db8c0dff933411ddc3704e89a
Piotr <posone89@gmail.com>
2020-02-01 20:19:13 +0100

    's'

---
+++ Dice Rolling Simulator/main.py
@@ -0,0 +1,10 @@
+from random import randint
+
+RANDOM_NUMBER = randint(1,6)
+for i in range(0,10):
+    RANDOM_NUMBER = randint(1,6)
+    print (RANDOM_NUMBER)
+
+while True:
+    print("Welcome to the DiceRollingSimulator! What do you want to do?")
+
